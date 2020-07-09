1799ed98a637d9dcfe8e781b0946821a674d345e
franec94 <franec94@gmail.com>
2020-07-09 11:34:32 +0200

    Added models with droput for experiment tests

--- dummy_toy_analyses/dnn_ananlyses/dnn_analyses_with_keras/reuters_reviews_utils/reuters_reviews_utils.R
+++ dummy_toy_analyses/dnn_ananlyses/dnn_analyses_with_keras/reuters_reviews_utils/reuters_reviews_utils.R
@@ -15,6 +15,24 @@ build_model <- function(input_shape) {
   )
 }
 
+
+build_model_with_dropout <- function(input_shape, dropout_rate = 0.5) {
+  model <- keras_model_sequential() %>%
+    layer_dense(units = 64, activation = "relu",
+                input_shape = input_shape) %>%
+    layer_dropout(rate = dropout_rate) %>%
+    layer_dense(units = 64, activation = "relu") %>%
+    layer_dropout(rate = dropout_rate) %>%
+    layer_dense(units = 46, activation = "softmax")
+  
+  model %>% compile(
+    optimizer = "rmsprop",
+    loss = "categorical_crossentropy",
+    metrics = c("accuracy")
+  )
+}
+
+
 build_small_model <- function(input_shape) {
   model <- keras_model_sequential() %>%
     layer_dense(units = 16, activation = "relu",
@@ -28,6 +46,24 @@ build_small_model <- function(input_shape) {
     metrics = c("accuracy")
   )
 }
+
+
+build_small_model_with_dropout <- function(input_shape, dropout_rate = 0.5) {
+  model <- keras_model_sequential() %>%
+    layer_dense(units = 16, activation = "relu",
+                input_shape = input_shape) %>%
+    layer_dropout(rate = dropout_rate) %>%
+    layer_dense(units = 16, activation = "relu") %>%
+    layer_dropout(rate = dropout_rate) %>%
+    layer_dense(units = 46, activation = "softmax")
+  
+  model %>% compile(
+    optimizer = "rmsprop",
+    loss = "categorical_crossentropy",
+    metrics = c("accuracy")
+  )
+}
+
 
 build_information_bottleneck_model <- function(input_shape) {
   model <- keras_model_sequential() %>%
@@ -43,6 +79,24 @@ build_information_bottleneck_model <- function(input_shape) {
   )
 }
 
+
+build_information_bottleneck_model_with_dropout <- function(input_shape, dropout_rate = 0.5) {
+  model <- keras_model_sequential() %>%
+    layer_dense(units = 64, activation = "relu",
+                input_shape = input_shape) %>%
+    layer_dropout(rate = dropout_rate) %>%
+    layer_dense(units = 4, activation = "relu") %>%
+    layer_dropout(rate = dropout_rate) %>%
+    layer_dense(units = 46, activation = "softmax")
+  
+  model %>% compile(
+    optimizer = "rmsprop",
+    loss = "categorical_crossentropy",
+    metrics = c("accuracy")
+  )
+}
+
+
 vectorize_sequences <- function(sequences, dimension = 10000) {
   results <- matrix(0, nrow = length(sequences), ncol = dimension)
   for (i in 1:length(sequence)) {
@@ -50,6 +104,7 @@ vectorize_sequences <- function(sequences, dimension = 10000) {
   }
   results
 }
+
 
 to_one_hot <- function(labels, dimension = 46) {
   results <- matrix(0, nrow = length(labels), ncol = dimension)
